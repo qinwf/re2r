519ce733b742c0edfaeb2cddddefeddfd45c3c04
qinwf <mail@qinwenfeng.com>
2016-01-20 16:56:31 +0800

    Fix: Fix for clang Rcpp::Rcerr << str_.str()

--- inst/include/util/logging.h
+++ inst/include/util/logging.h
@@ -123,7 +123,7 @@ public:
             // string s = str_.str();
             // size_t n = s.size();
             // if (fwrite(s.data(), 1, n, stderr) < n) {}  // shut up gcc
-            Rcpp::Rcerr << str_;
+            Rcpp::Rcerr << str_.str();
         }
         flushed_ = true;
     }
