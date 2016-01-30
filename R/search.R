f3e6f859ee84aff62d557a5da4154bad9aec9741
qinwf <mail@qinwenfeng.com>
2016-01-30 22:38:13 +0800

    remove: obj in result type

--- R/search.R
+++ R/search.R
@@ -1,22 +1,22 @@
 #' re2_match
 #' @param pattern a pre-compiled regular expression or a string
 #' @param string a character vector
-#' @param result "bool", "value", "object"
+#' @param value return value instead of bool result
 #' @param anchor "start": anchor match at the beginning of the string, "both": anchor match at the beginning and the end of the string, "none": no anchor.
 #' @param all find all match instead of first match. When all = FALSE and result = "value", the return type will be character vector when there is no capture group or data.frame.
 #' @param ... further arguments passed to or from other methods.
 #' @export
-re2_match = function(pattern, string, result = "bool", anchor = "start", all = TRUE, ...) UseMethod("re2_match")
+re2_match = function(pattern, string, value = FALSE, anchor = "none", all = TRUE, ...) UseMethod("re2_match")
 
 #' @rdname re2_match
 #' @export
-re2_match.re2exp = function(pattern, string, result = "bool", anchor = "start", all = TRUE, ...){
-
+re2_match.re2exp = function(pattern, string, value = FALSE, anchor = "none", all = TRUE, ...){
+    cpp_match(pattern, string, value, anchor, all)
 }
 
 #' @rdname re2_match
 #' @export
-re2_match.character = function(pattern, string, result = "bool", anchor = "start", all = TRUE, ...){
+re2_match.character = function(pattern, string, value = FALSE, anchor = "none", all = TRUE, ...){
     pattern = re2(pattern, ...)
-    re2_match.re2exp(pattern, string, result, anchor, all)
+    re2_match.re2exp(pattern, string, value, anchor, all)
 }
