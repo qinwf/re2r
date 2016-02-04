2e07cd3d3f2a1708a0c4712655759102cf2716a2
qinwf <mail@qinwenfeng.com>
2016-02-04 09:39:38 +0800

    Add: unit test extract

---
+++ tests/testthat/test-extract.R
@@ -0,0 +1,10 @@
+context("check rewrite")
+
+test_that("check rewrite", {
+    # from re2_test.cc
+    expect_identical(re2_extract("(.*)@([^.]*)", "boris@kremvax.ru","\\2!\\1"),"kremvax!boris")
+    expect_identical(re2_extract(".*", "foo","'\\0'"),"'foo'")
+    expect_identical(re2_extract("bar", "baz","'\\0'"),"'bar'")
+
+})
+
