1d441f6449d52756164835660789919bf09258a4
qinwf <mail@qinwenfeng.com>
2016-04-23 20:53:51 +0800

    Fix: ambiguous variable names

--- inst/include/re2r.h
+++ inst/include/re2r.h
@@ -45,7 +45,7 @@ using re2::StringPiece;
 #define RCPP_EXCEPTION_CLASS(__CLASS__,__WHAT__)                               \
     class __CLASS__ : public std::exception{                                       \
     public:                                                                        \
-        __CLASS__( const std::string& message ) throw() : message( __WHAT__ ){} ;  \
+        __CLASS__( const std::string& messages ) throw() : message( __WHAT__ ){} ;  \
         virtual ~__CLASS__() throw(){} ;                                           \
         virtual const char* what() const throw() { return message.c_str() ; }      \
     private:                                                                       \
@@ -60,37 +60,37 @@ using re2::StringPiece;
         virtual const char* what() const throw() { return __MESSAGE__ ; }          \
     } ;
 
-RCPP_EXCEPTION_CLASS(ErrorInternal, std::string("unexpected error:") + message)
+RCPP_EXCEPTION_CLASS(ErrorInternal, std::string("unexpected error:") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorBadEscape, std::string("bad escape sequence: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorBadEscape, std::string("bad escape sequence: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorBadCharRange, std::string("bad character class range: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorBadCharRange, std::string("bad character class range: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorBadCharClass, std::string("bad character class: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorBadCharClass, std::string("bad character class: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorMissingBracket, std::string("missing closing ]: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorMissingBracket, std::string("missing closing ]: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorMissingParen, std::string("missing closing ): ") + message)
+RCPP_EXCEPTION_CLASS(ErrorMissingParen, std::string("missing closing ): ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorTrailingBackslash, std::string("trailing \\ at end of regexp: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorTrailingBackslash, std::string("trailing \\ at end of regexp: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorRepeatArgument, std::string("repeat argument: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorRepeatArgument, std::string("repeat argument: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorRepeatSize, std::string("bad repetition argument: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorRepeatSize, std::string("bad repetition argument: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorRepeatOp, std::string("bad repetition operator: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorRepeatOp, std::string("bad repetition operator: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorBadPerlOp, std::string("bad perl operator: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorBadPerlOp, std::string("bad perl operator: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorBadUTF8, std::string("invalid UTF-8 in regexp: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorBadUTF8, std::string("invalid UTF-8 in regexp: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorBadNamedCapture, std::string("bad named capture group: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorBadNamedCapture, std::string("bad named capture group: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorPatternTooLarge, std::string("pattern too large (compile failed): ") + message)
+RCPP_EXCEPTION_CLASS(ErrorPatternTooLarge, std::string("pattern too large (compile failed): ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorRewriteString, std::string("rewrite string error: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorRewriteString, std::string("rewrite string error: ") + messages)
 
-RCPP_EXCEPTION_CLASS(ErrorAnchorType, std::string("anchor type error: ") + message)
+RCPP_EXCEPTION_CLASS(ErrorAnchorType, std::string("anchor type error: ") + messages)
 
 XPtr<RE2> cpp_re2_compile(const char* pattern,
                           bool log_errors_value,
