d1dd771c3f80f61cefc84379d6cf76117c82c296
qinwf <mail@qinwenfeng.com>
2016-05-23 11:41:46 +0800

    Add: staticdocs document

--- R/widget.R
+++ R/widget.R
@@ -13,6 +13,10 @@ remove_named = function(string){
 #' @param height the widget height
 #' @seealso https://regexper.com/documentation.html
 #' @examples
+#' # Skip on CRAN
+#'
+#' \dontrun{
+#'
 #' # US ZIP code
 #'
 #' show_regex("[0-9]{5}(?:-[0-9]{4})?")
@@ -32,6 +36,7 @@ remove_named = function(string){
 #' # major credit cards
 #'
 #' show_regex("^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6011[0-9]{12}|622((12[6-9]|1[3-9][0-9])|([2-8][0-9][0-9])|(9(([0-1][0-9])|(2[0-5]))))[0-9]{10}|64[4-9][0-9]{13}|65[0-9]{14}|3(?:0[0-5]|[68][0-9])[0-9]{11}|3[47][0-9]{13})*$")
+#' }
 #'
 #' @export
 show_regex <- function(pattern, width = NULL, height = NULL) {
---
+++ inst/staticdocs/index.r
@@ -0,0 +1,49 @@
+sd_section("Match",
+  "Regular Expression Related Functions",
+  c(
+    "re2_detect",
+    "re2_extract",
+    "re2_locate",
+    "re2_match",
+    "re2_replace",
+    "re2_split",
+    "re2_subset",
+    "re2_pdetect",
+    "re2_pextract",
+    "re2_plocate",
+    "re2_pmatch",
+    "re2_preplace",
+    "re2_psplit",
+    "re2_psubset"
+  )
+)
+
+sd_section("Compile RE2 Regular Expression",
+  "RE2 Object",
+  c(
+    "re2",
+    "get_expression_size",
+    "get_named_groups",
+    "get_number_of_groups",
+    "get_pattern",
+    "get_program_fanout",
+    "print.re2c"
+  )
+)
+
+sd_section("Operators",
+  "Regular Expression Operators",
+  c(
+    "%=~%",
+    "%<~%"
+  )
+)
+
+sd_section("Utilities",
+  "Helper Functions",
+  c(
+    "show_regex",
+    "quote_meta",
+    "sub_string"
+  )
+)
