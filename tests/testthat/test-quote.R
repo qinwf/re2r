9358181cbe2fe1dfba2349d5aba6f94fcb96ce48
qinwf <mail@qinwenfeng.com>
2016-06-02 21:29:29 +0800

    Test: add quote_meta parallel test cases

--- tests/testthat/test-quote.R
+++ tests/testthat/test-quote.R
@@ -6,5 +6,7 @@ test_that("quote", {
     expect_identical(quote_meta("((a|b)c?d*e+[f-h]i)"),"\\(\\(a\\|b\\)c\\?d\\*e\\+\\[f\\-h\\]i\\)")
     expect_identical(quote_meta("foo.bar",parallel = T), "foo\\.bar")
     expect_identical(quote_meta("((a|b)c?d*e+[f-h]i)", parallel = T),"\\(\\(a\\|b\\)c\\?d\\*e\\+\\[f\\-h\\]i\\)")
+
+    expect_identical(quote_meta(c("((a|b)c?d*e+[f-h]i)","((a|b)c?d*e+[f-h]i)"), parallel = T, grain_size = 1),c("\\(\\(a\\|b\\)c\\?d\\*e\\+\\[f\\-h\\]i\\)", "\\(\\(a\\|b\\)c\\?d\\*e\\+\\[f\\-h\\]i\\)"))
 })
 
