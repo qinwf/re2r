821397b94b47064252bd6e8758535960c57a205a
qinwf <mail@qinwenfeng.com>
2016-06-03 07:55:25 +0800

    Test: show_regex error test case with number

--- tests/testthat/test-show.R
+++ tests/testthat/test-show.R
@@ -4,4 +4,5 @@ test_that("test show_regex",{
     show_regex(re2("(?P<testname>>this)( is)(?P<testname>>this)"))
     show_regex("show_regex")
     expect_error(show_regex(list()))
+    expect_error(show_regex(1))
 })
