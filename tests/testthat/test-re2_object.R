94d4923bc4dfbd063e8878e94f98ba1d651bf2e2
qinwf <mail@qinwenfeng.com>
2016-06-02 17:49:53 +0800

    Test: add print.re2c test cases

--- tests/testthat/test-re2_object.R
+++ tests/testthat/test-re2_object.R
@@ -29,7 +29,10 @@ test_that("number of groups",{
                             )
         ,100000L
     )
+})
 
+test_that("other RE2 object tests",{
     get_expression_size(re2("abc"))
     get_program_fanout(re2("^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6011[0-9]{12}|622((12[6-9]|1[3-9][0-9])|([2-8][0-9][0-9])|(9(([0-1][0-9])|(2[0-5]))))[0-9]{10}|64[4-9][0-9]{13}|65[0-9]{14}|3(?:0[0-5]|[68][0-9])[0-9]{11}|3[47][0-9]{13})*$"))
+    expect_output(print(re2("abc")),"re2 pre-compiled regular expression.*pattern: abc.*number of capturing subpatterns: 0.*capturing names with indices:.*expression size:")
 })
