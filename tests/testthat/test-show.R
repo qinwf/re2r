d62e1f535981a51b3b2273242733bad3edf46a5b
qinwf <mail@qinwenfeng.com>
2016-06-02 18:25:27 +0800

    Test: add character vector input test case for show_regex

--- tests/testthat/test-show.R
+++ tests/testthat/test-show.R
@@ -2,5 +2,6 @@ context("test show regex")
 
 test_that("test show_regex",{
     show_regex(re2("(?P<testname>>this)( is)(?P<testname>>this)"))
+    show_regex("show_regex")
     expect_error(show_regex(list()))
 })
