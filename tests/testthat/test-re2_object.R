dcb97f20c286b6c36758d007ccc9d3135b7ef68a
qinwf <mail@qinwenfeng.com>
2016-06-03 10:40:59 +0800

    Test: add posix syntax test cases

--- tests/testthat/test-re2_object.R
+++ tests/testthat/test-re2_object.R
@@ -36,3 +36,16 @@ test_that("other RE2 object tests",{
     get_program_fanout(re2("^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6011[0-9]{12}|622((12[6-9]|1[3-9][0-9])|([2-8][0-9][0-9])|(9(([0-1][0-9])|(2[0-5]))))[0-9]{10}|64[4-9][0-9]{13}|65[0-9]{14}|3(?:0[0-5]|[68][0-9])[0-9]{11}|3[47][0-9]{13})*$"))
     expect_output(print(re2("abc")),"re2 pre-compiled regular expression.*pattern: abc.*number of capturing subpatterns: 0.*capturing names with indices:.*expression size:")
 })
+
+library(stringi)
+
+test_that("posix syntax",{
+    expect_true(re2_detect("abc" , re2("abc", posix_syntax = T)))
+    expect_error(re2("(?P<name>re)", posix_syntax = T))
+})
+
+test_that("compile special cases",{
+    expect_null(re2(NULL))
+    expect_null(re2(NA))
+    expect_true(is.list(re2(c("as","ab","cs"))))
+})
