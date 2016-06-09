188736a518752a6dd1d3d3edb4d1d9fe908238ae
qinwf <mail@qinwenfeng.com>
2016-06-09 20:35:22 +0800

    Add: hashword for 32 bit Windows

--- src/hash.cc
+++ src/hash.cc
@@ -147,40 +147,40 @@ namespace re2 {
  hashlittle() has to dance around fitting the key bytes into registers.
 --------------------------------------------------------------------
 */
-// uint32 hashword(
-// const uint32 *k,                   /* the key, an array of uint32_t values */
-// size_t          length,               /* the length of the key, in uint32_ts */
-// uint32        initval)         /* the previous hash, or an arbitrary value */
-// {
-//   uint32_t a,b,c;
-//
-//   /* Set up the internal state */
-//   a = b = c = 0xdeadbeef + (((uint32_t)length)<<2) + initval;
-//
-//   /*------------------------------------------------- handle most of the key */
-//   while (length > 3)
-//   {
-//     a += k[0];
-//     b += k[1];
-//     c += k[2];
-//     mix(a,b,c);
-//     length -= 3;
-//     k += 3;
-//   }
-//
-//   /*------------------------------------------- handle the last 3 uint32_t's */
-//   switch(length)                     /* all the case statements fall through */
-//   {
-//   case 3 : c+=k[2];
-//   case 2 : b+=k[1];
-//   case 1 : a+=k[0];
-//     final(a,b,c);
-//   case 0:     /* case 0: nothing left to add */
-//     break;
-//   }
-//   /*------------------------------------------------------ report the result */
-//   return c;
-// }
+uint32 hashword(
+const uint32 *k,                   /* the key, an array of uint32_t values */
+size_t          length,               /* the length of the key, in uint32_ts */
+uint32        initval)         /* the previous hash, or an arbitrary value */
+{
+  uint32_t a,b,c;
+
+  /* Set up the internal state */
+  a = b = c = 0xdeadbeef + (((uint32_t)length)<<2) + initval;
+
+  /*------------------------------------------------- handle most of the key */
+  while (length > 3)
+  {
+    a += k[0];
+    b += k[1];
+    c += k[2];
+    mix(a,b,c);
+    length -= 3;
+    k += 3;
+  }
+
+  /*------------------------------------------- handle the last 3 uint32_t's */
+  switch(length)                     /* all the case statements fall through */
+  {
+  case 3 : c+=k[2];
+  case 2 : b+=k[1];
+  case 1 : a+=k[0];
+    final(a,b,c);
+  case 0:     /* case 0: nothing left to add */
+    break;
+  }
+  /*------------------------------------------------------ report the result */
+  return c;
+}
 
 
 /*
