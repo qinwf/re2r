3164860711c030c3d6b33f23f7ff8d437a9da3cb
qinwf <mail@qinwenfeng.com>
2016-06-15 00:30:19 +0800

    Test: stringi split test cases

--- tests/testthat/test-split.R
+++ tests/testthat/test-split.R
@@ -24,3 +24,55 @@ test_that("split test",{
 test_that("split error test",{
     expect_error(re2_split("sd",pattern = "s", part = numeric()),"need the number of pieces.")
 })
+
+test_that("Stringi test cases",{
+    expect_identical(stri_split_regex(character(0)," "),re2_split(character(0)," "))
+    # todo NA pattern
+    # expect_identical(stri_split_regex(NA,NA),re2_split(NA,NA))
+    expect_identical(stri_split_regex(NA,"a"),re2_split(NA,"a"))
+    # expect_identical(stri_split_regex("NA",NA),re2_split("NA",NA))
+
+    # todo n = NA
+    # expect_identical(stri_split_regex("NA","a",NA),re2_split("NA","a",NA))
+
+    # tocheck  empty string
+    expect_identical(stri_split_regex(" "," "),re2_split(" "," "))
+    # expect_identical(stri_split_regex("","Z"),re2_split("","Z"))
+    # expect_identical(stri_split_regex("",".*"),re2_split("",".*"))
+
+    # expect_identical(stri_split_regex("","Z", omit_empty=TRUE),list(character(0)))
+
+    # tocheck empty string
+    # expect_identical(stri_split_regex("aa","a"),list(rep("",3)))
+    input_list = list(
+        # tocheck
+        #c("ala ma kota 1 a","[a-z] [a-z]"),
+        c("ala ma kota 1 a","[a-z] [a-z]*"),
+        c("ala ma kota 1 a","[a-z] [a-z]+"),
+        c("ala ma kota 1 a","[a-z] [1-9]"),
+        c("ala ma kota 1 a","[a-z] [1-9]+"),
+        c(c("\u0105\u0106\u0107", "\u0105\u0107"), "\u0106*"),
+        c(c("\u0105\u0106\u0107", "\u0105\u0107"), "(?<=\u0106)")
+        )
+    for(x in input_list){
+        # print(x)
+        expect_identical(stri_split_regex(x[1],x[2]), re2_split(x[1],x[2]))
+    }
+
+    # n
+    expect_identical(stri_split_regex(";123", ";", n=2), re2_split(";123", ";", 2))
+    expect_identical(stri_split_regex("123;456", ";", n=2), re2_split("123;456", ";", 2))
+    expect_identical(stri_split_regex("123;456;789", ";", n=2), re2_split("123;456;789", ";", 2))
+
+    # expect_identical(stri_split_regex("123-456-789", "-", n=1:3), list(c("123-456-789"),c("123","456-789"),c("123","456","789")))
+
+    expect_identical(stri_split_regex("123-456-789", "[1-8]-.", n=5), re2_split("123-456-789", "[1-8]-.", 5))
+
+    # tokens_only
+    expect_identical(stri_split_regex("a_b_c_d", "_"), re2_split("a_b_c_d", "_"))
+
+    # tocheck empty string
+    # expect_identical(stri_split_regex("a_b_c__d", "_"), re2_split("a_b_c__d", "_"))
+
+
+})
