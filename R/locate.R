7910bd263422e9fd8c1128c72696fd4dc7c2a793
qinwf <mail@qinwenfeng.com>
2016-07-02 09:28:27 +0800

    Change: merge plocate functions to locate

--- R/locate.R
+++ R/locate.R
@@ -33,56 +33,27 @@
 #' If the match is of length 0, (e.g. from a special match like $) end will be one character less than start.
 #'
 #' @param pattern a pre-compiled regular expression or a string
-#' @param input a character vector
+#' @param string a character vector
+#' @param parallel use multithread
+#' @param grain_size a minimum chunk size for tuning the behavior of parallel algorithms
 #' @param ... further arguments passed to or from other methods.
 #' @examples
 #' re2_locate("yabba dabba doo", "d")
 #' @export
-re2_locate = function(input, pattern, ...) {
+re2_locate = function(string, pattern,  parallel = FALSE, grain_size= 100000, ...) {
 
     if (is.character(pattern)) {
         pattern = re2(pattern, ...)
     }
-    cpp_locate(stri_enc_toutf8(input), pattern, FALSE, FALSE,1)
+    cpp_locate(stri_enc_toutf8(string), pattern, FALSE, parallel, grain_size)
 }
 
 #' @rdname re2_locate
 #' @export
-re2_locate_all = function(input, pattern, ...) {
+re2_locate_all = function(string, pattern,  parallel = FALSE, grain_size= 100000, ...) {
 
     if (is.character(pattern)) {
         pattern = re2(pattern, ...)
     }
-    cpp_locate(stri_enc_toutf8(input), pattern, TRUE, FALSE,1)
-}
-
-#' Locate a pattern in strings with multithread.
-#'
-#' Like re2_preplace, except that if the pattern matches, substring is copied into the result. The non-matching
-#' portions of "text" are ignored.
-#'
-#' @param pattern a pre-compiled regular expression or a string
-#' @param input a character vector
-#' @param grain_size a minimum chunk size for tuning the behavior of parallel algorithms.
-#' @param ... further arguments passed to or from other methods.
-#' @examples
-#' re2_plocate("yabba dabba doo", "(d)")
-#' @export
-re2_plocate = function(input, pattern,
-                       grain_size = 100000, ...) {
-    if (is.character(pattern)) {
-        pattern = re2(pattern, ...)
-    }
-
-    cpp_locate(stri_enc_toutf8(input), pattern, FALSE, TRUE,grain_size)
-}
-
-#' @rdname re2_pextract
-#' @export
-re2_plocate_all = function(input, pattern,
-                           grain_size = 100000, ...) {
-    if (is.character(pattern)) {
-        pattern = re2(pattern, ...)
-    }
-    cpp_locate(stri_enc_toutf8(input), pattern, TRUE, TRUE,grain_size)
+    cpp_locate(stri_enc_toutf8(string), pattern, TRUE, parallel, grain_size)
 }
