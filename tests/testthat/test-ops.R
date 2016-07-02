47c37fa140ca533ec12a21009371df8995cb67c0
qinwf <mail@qinwenfeng.com>
2016-07-02 09:25:17 +0800

    Remove: operators
    These operators are unreadable for new users.

--- tests/testthat/test-ops.R
+++
@@ -1,9 +0,0 @@
-context("operators")
-
-test_that("test operators",{
-    regexp %<~% "(12)"
-    expect_true(re2_detect("12",regexp))
-    expect_identical(get_pattern(regexp),"(12)")
-    expect_true(!("12" %!~% regexp))
-    expect_true(  "12" %=~% regexp)
-})
