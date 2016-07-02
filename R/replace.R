77fd648a39c1b98df8cacd5be4ffcdbaf2704a8b
qinwf <mail@qinwenfeng.com>
2016-07-02 09:48:45 +0800

    Change: merge preplace with replace

--- R/replace.R
+++ R/replace.R
@@ -35,66 +35,56 @@
 #' Within "rewrite", backslash-escaped digits (\\1 to \\9) can be
 #' used to insert text matching corresponding parenthesized group
 #' from the pattern.  \\0 in "rewrite" refers to the entire matching
-#' text.
+#' text. Vectorised over string, pattern and replacement.
 #'
 #' @param pattern a pre-compiled regular expression or a string
-#' @param rewrite replace the first match or all of the match of "pattern" in "input" with "rewrite"
-#' @param input a character vector
-#' @param ... further arguments passed to or from other methods.
+#' @param replacement replace the first match or all of the match of "pattern" in "input" with "rewrite"
+#' @param string input character vector
+#' @param parallel use multithread
+#' @param grain_size a minimum chunk size for tuning the behavior of parallel algorithms
+#' @param ... further arguments passed to or from other methods
 #' @return a character vector
 #' @examples
 #' regexp = re2("b+")
 #' re2_replace_all("yabba dabba doo", regexp,"d") == "yada dada doo"
 #' re2_replace("yabba dabba doo", "b+","d") == "yada dabba doo"
 #' @export
-re2_replace = function(input, pattern, rewrite, ...) {
+re2_replace = function(string,
+                       pattern,
+                       replacement,
+                       parallel = FALSE,
+                       grain_size = 100000,
+                       ...) {
     if (is.character(pattern)) {
         pattern = re2(pattern, ...)
     }
-    cpp_replace(stri_enc_toutf8(input), pattern, stri_enc_toutf8(rewrite),  FALSE, FALSE,1)
+    cpp_replace(
+        stri_enc_toutf8(string),
+        pattern,
+        stri_enc_toutf8(replacement),
+        FALSE,
+        parallel ,
+        grain_size
+    )
 }
 
 #' @rdname re2_replace
 #' @export
-re2_replace_all = function(input, pattern, rewrite, ...) {
+re2_replace_all = function(string,
+                           pattern,
+                           replacement,
+                           parallel = FALSE,
+                           grain_size = 100000,
+                           ...) {
     if (is.character(pattern)) {
         pattern = re2(pattern, ...)
     }
-    cpp_replace(stri_enc_toutf8(input), pattern, stri_enc_toutf8(rewrite), TRUE, FALSE,1)
-}
-
-#' Replace a pattern in strings with multithread.
-#'
-#' Replace the first match of "pattern" in "str" with "rewrite".
-#' Within "rewrite", backslash-escaped digits (\\1 to \\9) can be
-#' used to insert text matching corresponding parenthesized group
-#' from the pattern.  \\0 in "rewrite" refers to the entire matching
-#' text.
-#'
-#' @param pattern a pre-compiled regular expression or a string
-#' @param rewrite replace the first match or all of the match of "pattern" in "input" with "rewrite"
-#' @param input a character vector
-#' @param grain_size a minimum chunk size for tuning the behavior of parallel algorithms.
-#' @param ... further arguments passed to or from other methods.
-#'
-#' @return a character vector
-#' @examples
-#' regexp = re2("b+")
-#' re2_preplace_all("yabba dabba doo", regexp,"d") == "yada dada doo"
-#' re2_preplace("yabba dabba doo", "b+","d") == "yada dabba doo"
-#' @export
-re2_preplace = function(input, pattern, rewrite , grain_size = 100000, ...) {
-    if (is.character(pattern)) {
-        pattern = re2(pattern, ...)
-    }
-    cpp_replace(stri_enc_toutf8(input), pattern, stri_enc_toutf8(rewrite),  FALSE, TRUE, grain_size)
-}
-
-#' @rdname re2_preplace
-#' @export
-re2_preplace_all = function(input, pattern, rewrite, grain_size = 100000, ...) {
-    if (is.character(pattern)) {
-        pattern = re2(pattern, ...)
-    }
-    cpp_replace(stri_enc_toutf8(input), pattern, stri_enc_toutf8(rewrite),  TRUE, TRUE, grain_size)
+    cpp_replace(
+        stri_enc_toutf8(string),
+        pattern,
+        stri_enc_toutf8(replacement),
+        TRUE,
+        parallel ,
+        grain_size
+    )
 }
