b9f8fd672623c85662aba8a579924c8eb684fdee
qinwf <mail@qinwenfeng.com>
2016-07-03 16:12:24 +0800

    Update: tidy check rewrite tests

--- tests/testthat/test-check_rewrite.R
+++ tests/testthat/test-check_rewrite.R
@@ -1,31 +1,40 @@
 context("check rewrite")
 
 test_that("check rewrite", {
-    # from re2_test.cc
-    lists = list(
-        list("abc", c("foo", TRUE),
-             c("foo\\", FALSE), c("foo\\0bar", TRUE)),
-
-        list("a(b)c", c("foo", TRUE), c("foo\\0bar", TRUE),
-             c( "foo\\1bar", TRUE), c("foo\\2bar", FALSE),
-             c("f\\\\2o\\1o", TRUE)),
-
-        list("a(b)(c)", c("foo\\12", TRUE),
-             c( "f\\2o\\1o", TRUE), c("f\\oo\\1", FALSE))
-    )
-
-
-    for (ind in lists) {
-        regexp = re2(ind[[1]])
+    tt = function(ind) {
         for (exp in 2:length(ind)) {
             if (ind[[exp]][2] == "FALSE") {
-                expect_error(re2_replace("a", ind[[1]], ind[[exp]][1] ) ,
+                expect_error(re2_replace("a", ind[[1]], ind[[exp]][1]) ,
                              "rewrite string error")
-            } else { # "TRUE"
-                expect_equal( re2_replace("q", ind[[1]], ind[[exp]][1]), "q")
+            } else {
+                # "TRUE"
+                expect_equal(re2_replace("q", ind[[1]], ind[[exp]][1]), "q")
             }
         }
     }
 
-})
 
+    tt(list(
+        "abc",
+        c("foo", TRUE),
+        c("foo\\", FALSE),
+        c("foo\\0bar", TRUE)
+    ))
+
+    tt(list(
+        "a(b)c",
+        c("foo", TRUE),
+        c("foo\\0bar", TRUE),
+        c("foo\\1bar", TRUE),
+        c("foo\\2bar", FALSE),
+        c("f\\\\2o\\1o", TRUE)
+    ))
+
+    tt(list(
+        "a(b)(c)",
+        c("foo\\12", TRUE),
+        c("f\\2o\\1o", TRUE),
+        c("f\\oo\\1", FALSE)
+    ))
+
+})
