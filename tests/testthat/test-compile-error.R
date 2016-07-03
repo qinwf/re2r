094ca00698cfe97ccf7edb8d28b936b84dd893aa
qinwf <mail@qinwenfeng.com>
2016-07-03 15:30:34 +0800

    Update: tidy compile error tests

--- tests/testthat/test-compile-error.R
+++ tests/testthat/test-compile-error.R
@@ -1,47 +1,43 @@
 context("compile errors")
 library(stringi)
 
-test_that("compile errors",{
-    expect_error(re2("\\",log_error = T), "trailing \\\\ at end of regexp: ")
-    expect_error(re2("\\2",log_error = T), "bad escape sequence: ")
-    expect_error(re2("[[:prin:]]",log_error = T), "bad character class range:")
-    expect_error(re2("[a-4]",log_error = T), "bad character class range")
-    expect_error(re2("(sd",log_error = T), "missing closing ")
-    expect_error(re2("[[:print:]",log_error = T),"missing closing")
-    expect_error(re2("[[:print:]]++",log_error = T),"bad repetition operator")
-    expect_error(re2("[[:print:]]{3}{3}",log_error = T), "bad repetition operator")
-    expect_error(re2("(?P<.sd>sd",log_error = T), "bad named capture group")
-    expect_error(re2(stri_dup("a",1000000),log_error = T), "pattern too large ")
-
-    expect_error(re2("(((.{100}){100}){100}){100}"), "bad repetition argument")
-    expect_error(re2("a\\"),"trailing")
-    expect_error(re2("a[x"),"missing closing")
-    expect_error(re2("a[z-a]"),"bad character class range:")
-    expect_error(re2("a[[:foobar:]]"),"bad character class range:")
-
-    error_cases = list(
-        c( stri_enc_fromutf32(c(97L, 98L, 92L, 945L, 99L, 100L)), "bad escape sequence:" ),
-        c( stri_enc_fromutf32(c(101L, 102L, 92L, 120L, 9786L, 48L, 49L)), "bad escape sequence" ),
-        c( stri_enc_fromutf32(c(103L, 104L, 92L, 120L, 49L, 9786L, 48L, 49L)), "bad escape sequence" ),
-        c( "ij\\x1", "bad escape sequence" ),
-        c( "kl\\x", "bad escape sequence" ),
-        c( stri_enc_fromutf32(c(117L, 118L, 92L, 120L, 123L, 48L, 48L, 48L, 48L, 9786L, 125L
-        )), "bad escape sequence" ),
-        c( "wx\\p{ABC", "bad character class range" ),
-        c( "yz(?smiUX:abc)", "bad perl operator" ),  # used to return (?s but the error is X
-        c( stri_enc_fromutf32(c(97L, 97L, 40L, 63L, 115L, 109L, 9786L, 105L)), "bad perl operator" ),
-        c( "bb[abc", "missing closing" ),
-
-        c( "mn\\x1\377", "" ),  # no argument string returned for invalid UTF-8
-        # c( "op\377qr", "" ),
-        c( "st\\x{00000\377", "" ),
-        c( "zz\\p{\377}", "" ),
-        c( "zz\\x{00\377}", "" ),
-        c( "zz(?P<name\377>abc)", "" )
-    )
-    for ( x in error_cases ){
-        print(x)
-        expect_error(re2(x[1]), x[2])
+test_that("compile errors", {
+    tt = function(pattern, output_string) {
+        expect_output(expect_error(re2(pattern, log_error = T), output_string))
     }
-})
+    tt("\\" , "trailing \\\\ at end of regexp: ")
+    tt("\\2", "bad escape sequence: ")
+    tt("[[:prin:]]", "bad character class range:")
+    tt("[a-4]", "bad character class range")
+    tt("(sd", "missing closing")
+    tt("[[:print:]", "missing closing")
+    tt("[[:print:]]++", "bad repetition operator")
+    tt("[[:print:]]{3}{3}", "bad repetition operator")
+    tt("(?P<.sd>sd",  "bad named capture group")
+    tt(stri_dup("a", 1000000), "pattern too large")
 
+    tt("(((.{100}){100}){100}){100}", "bad repetition argument")
+    tt("a\\", "trailing")
+    tt("a[x", "missing closing")
+    tt("a[z-a]", "bad character class range:")
+    tt("a[[:foobar:]]", "bad character class range:")
+    tt(stri_enc_fromutf32(c(97L, 98L, 92L, 945L, 99L, 100L)), "bad escape sequence:")
+    tt(stri_enc_fromutf32(c(101L, 102L, 92L, 120L, 9786L, 48L, 49L)), "bad escape sequence")
+    tt(stri_enc_fromutf32(c(103L, 104L, 92L, 120L, 49L, 9786L, 48L, 49L
+    )), "bad escape sequence")
+    tt("ij\\x1", "bad escape sequence")
+    tt("kl\\x", "bad escape sequence")
+    tt(stri_enc_fromutf32(c(117L, 118L, 92L, 120L, 123L, 48L, 48L, 48L, 48L, 9786L, 125L)), "bad escape sequence")
+    tt("wx\\p{ABC", "bad character class range")
+    tt("yz(?smiUX:abc)", "bad perl operator")
+    # used to return (?s but the error is X
+    tt(stri_enc_fromutf32(c(97L, 97L, 40L, 63L, 115L, 109L, 9786L, 105L)), "bad perl operator")
+    tt("bb[abc", "missing closing")
+    tt("mn\\x1\377", "")
+    # no argument string returned for invalid UTF-8
+    # c( "op\377qr", "" ),
+    tt("st\\x{00000\377", "")
+    tt("zz\\p{\377}", "")
+    tt("zz\\x{00\377}", "")
+    tt("zz(?P<name\377>abc)", "")
+})
