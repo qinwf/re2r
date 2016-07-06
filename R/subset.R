7babaece65a3fcf948c31e9b5798ce7220e56093
qinwf <mail@qinwenfeng.com>
2016-07-06 13:16:30 +0800

    Change: merge subset with psubset

--- R/subset.R
+++ R/subset.R
@@ -45,43 +45,33 @@
 #' re2_subset(fruit, "[aeiou]")
 #'
 #' re2_subset(c("a", NA, "b"), ".")
-re2_subset <- function(input, pattern, anchor = 0, ...) {
-    input[re2_detect(input, pattern ,anchor, ...)]
-}
+re2_subset <-
+    function(input,
+             pattern,
+             anchor = 0,
+             parallel = FALSE,
+             grain_size = 100000,
+             ...) {
+        input[re2_detect(input, pattern , anchor, parallel, grain_size, ...)]
+    }
 
 #' @rdname re2_subset
 #' @export
-`re2_subset<-` <- function(input, pattern, anchor = 0, ..., value){
-    res_bool = re2_detect(input, pattern ,anchor)
-    res_bool[is.na(res_bool)] = FALSE
-    if (all(!res_bool)){
-        return(input)
+`re2_subset<-` <-
+    function(input,
+             pattern,
+             anchor = 0,
+             parallel,
+             grain_size,
+             ...,
+             value) {
+        res_bool = re2_detect(input, pattern , anchor, parallel, grain_size, ...)
+        res_bool[is.na(res_bool)] = FALSE
+
+        if (all(!res_bool)) {
+            return(input)
+        }
+
+        input[res_bool] <- value
+        input
     }
-    input[res_bool] <- value
-    input
-}
-
-#' Keep strings matching a pattern with multithread.
-#'
-#' This is a convenient wrapper around \code{x[re2_detect(x, pattern)]}.
-#'
-#' @inheritParams re2_pdetect
-#' @param value character vector to be substituted with; replacement function only
-#' @return A character vector.
-#' @export
-re2_psubset <- function(input, pattern, anchor = 0, grain_size = 100000, ...) {
-    input[re2_pdetect(input, pattern ,anchor, grain_size, ...)]
-}
-
-#' @rdname re2_psubset
-#' @export
-`re2_psubset<-` = function(input, pattern, anchor = 0, ..., value){
-    res_bool = re2_pdetect(input, pattern ,anchor)
-    res_bool[is.na(res_bool)] = FALSE
-    if (all(!res_bool,na.rm = T)){
-        return(input)
-    }
-    input[res_bool] <- value
-    input
-}
-
