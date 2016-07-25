b0fbf021fda5804988d1e95a39e357983665e2e4
qinwf <mail@qinwenfeng.com>
2016-07-25 09:27:11 +0800

    Test: tidy replace tests

--- tests/testthat/test-replace.R
+++ tests/testthat/test-replace.R
@@ -2,96 +2,111 @@ context("check replace")
 
 test_that("re2_replace", {
     # from re2_test.cc
-    replace_list = list(
-        c( "(qu|[b-df-hj-np-tv-z]*)([a-z]+)", # regexp
+    tt = function(pattern, replacement,string,res, res_all, num){
+        expect_identical(re2_replace(string, pattern, replacement), res)
+        expect_identical(re2_replace(string, pattern, replacement ,parallel = T), res)
+        expect_identical(re2_replace(string, pattern, replacement ,parallel = T, grain_size = 1), res)
+
+        res = re2_replace_all(string, pattern,replacement)
+        expect_identical(as.character(res), res_all)
+        expect_identical(attr(res, "count"), as.numeric(num))
+
+        pres = re2_replace_all(string, pattern, replacement, parallel= T, grain_size = 1)
+        expect_identical(as.character(pres), res_all)
+        expect_identical(attr(pres, "count"), as.numeric(num))
+    }
+
+        tt("(qu|[b-df-hj-np-tv-z]*)([a-z]+)", # regexp
            "\\2\\1ay", # rewrite
            "the quick brown fox jumps over the lazy dogs.", # origin
            "ethay quick brown fox jumps over the lazy dogs.", #result
            "ethay ickquay ownbray oxfay umpsjay overay ethay azylay ogsday.", #global
            9 # greplace_count
-        ),
-        c("\\w+",
+        )
+        tt("\\w+",
           "\\0-NOSPAM",
           "abcd.efghi@google.com",
           "abcd-NOSPAM.efghi@google.com",
           "abcd-NOSPAM.efghi-NOSPAM@google-NOSPAM.com-NOSPAM",
-          4 ),
-        c("^",
+          4 )
+        tt("^",
           "(START)",
           "foo",
           "(START)foo",
           "(START)foo",
-          1 ),
-        c("^",
+          1 )
+        tt("^",
           "(START)",
           "",
           "(START)",
           "(START)",
-          1 ),
-        c( "$",
+          1 )
+        tt( "$",
            "(END)",
            "",
            "(END)",
            "(END)",
-           1 ),
-        c( "b",
+           1 )
+        tt( "b",
            "bb",
            "ababababab",
            "abbabababab",
            "abbabbabbabbabb",
-           5 ),
-        c( "b",
+           5 )
+        tt( "b",
            "bb",
            "bbbbbb",
            "bbbbbbb",
            "bbbbbbbbbbbb",
-           6 ),
-        c( "b+",
+           6 )
+        tt( "b+",
            "bb",
            "bbbbbb",
            "bb",
            "bb",
-           1 ),
-        c( "b*",
+           1 )
+        tt( "b*",
            "bb",
            "bbbbbb",
            "bb",
            "bb",
-           1 ),
-        c( "b*",
+           1 )
+        tt( "b*",
            "bb",
            "aaaaa",
            "bbaaaaa",
            "bbabbabbabbabbabb",
-           6 ),
+           6 )
         # Check newline handling
-        c( "a.*a",
+        tt( "a.*a",
            "(\\0)",
            "aba\naba",
            "(aba)\naba",
            "(aba)\n(aba)",
-           2 ))
+           2 )
 
-    for (ind in replace_list) {
-
-        expect_identical(re2_replace(ind[3], ind[1], ind[2]), ind[4])
-        expect_identical(re2_preplace(ind[3], ind[1], ind[2]), ind[4])
-
-        res = re2_replace_all(ind[3], ind[1],ind[2])
-        expect_identical(as.character(res), ind[5])
-        expect_identical(attr(res, "count"), as.numeric(ind[6]))
-
-        pres = re2_preplace_all(ind[3], ind[1],ind[2])
-        expect_identical(as.character(pres), ind[5])
-        expect_identical(attr(pres, "count"), as.numeric(ind[6]))
-
-    }
-
-    expect_identical(re2_preplace(input = c("a","aa", NA), pattern = "a", rewrite = "b", grain_size = 1), c("b","ba", NA))
-    expect_identical(re2_preplace_all(input = c("a","aa", NA), pattern = "a", rewrite = "b", grain_size = 1), structure(c("b", "bb", NA), count = c(1, 2, 0)))
+    expect_identical(re2_replace(string = c("a","aa", NA), pattern = "a", replacement  = "b", grain_size = 1), c("b","ba", NA))
 })
 
 test_that("replce vectorize",{
+    tt = function(pattern, replacement,string,res, res_all, num, warns) {
+
+        expect_identical(suppressWarnings(re2_replace(string, pattern, replacement)), res)
+        expect_identical(suppressWarnings(re2_replace(string, pattern, replacement)), res, parallel = T)
+        expect_true(identical(suppressWarnings(re2_preplace(string, pattern, replacement, parallel = T, grain_size = 1) ) , suppressWarnings(re2_replace(string, pattern, replacement)))
+        )
+
+        res = suppressWarnings(re2_replace_all(string, pattern, replacement))
+        expect_identical(as.character(res), res_all)
+        expect_identical(attr(res, "count"), num)
+        expect_identical(res_all, suppressWarnings(re2_replace_all(string, pattern, replacement, parallel = T, grain_size = 1)))
+
+        # vectorize warning
+        if(ind[[7]] == TRUE){
+            expect_warning(re2_replace_all(string, pattern, replacement))
+        }
+
+    }
     replace_list = list(
         list( c("a+", "b"),
               c("x", "y"),
@@ -130,23 +145,4 @@ test_that("replce vectorize",{
               rep(c(4,1,2),1000) ,
               FALSE)
     )
-
-    for (ind in replace_list) {
-
-        expect_identical(suppressWarnings(re2_replace(ind[[3]], ind[[1]], ind[[2]])), ind[[4]])
-        expect_identical(suppressWarnings(re2_preplace(ind[[3]], ind[[1]], ind[[2]])), ind[[4]])
-        expect_true(identical(suppressWarnings(re2_preplace(ind[[3]], ind[[1]], ind[[2]],grain_size = 1) ) , suppressWarnings(re2_replace(ind[[3]], ind[[1]], ind[[2]])))
-        )
-
-        res = suppressWarnings(re2_replace_all(ind[[3]], ind[[1]], ind[[2]]))
-        expect_identical(as.character(res), ind[[5]])
-        expect_identical(attr(res, "count"), ind[[6]])
-        expect_identical(res, suppressWarnings(re2_preplace_all(ind[[3]], ind[[1]], ind[[2]], grain_size = 1)))
-
-        # vectorize warning
-        if(ind[[7]] == TRUE){
-            expect_warning(re2_replace_all(ind[[3]], ind[[1]], ind[[2]]))
-        }
-
-    }
 })
