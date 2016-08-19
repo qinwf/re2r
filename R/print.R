470619004cfa154ea55d787c7da63f68bb3daeb6
qinwf <mail@qinwenfeng.com>
2016-08-19 12:31:00 +0800

    Update: more examples for re2 function

--- R/print.R
+++ R/print.R
@@ -28,32 +28,50 @@
 ## OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 ## EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
+cat_lines = function(des, res){
+    cat(des)
+    cat(res)
+    cat("\n")
+}
 
 #' Print information about a pre-compiled regular expression
 #' @param x a pre-compiled regular expression
+#' @param options print options
 #' @param ... further arguments passed to or from other methods.
 #' @examples
 #' re2("(.*)@([^.]*)")
 #' re2("(?P<name>sd)")
+#' print(re2("sd"), options = TRUE)
 #' @export
-print.re2c = function(x, ...) {
+print.re2c = function(x, options = FALSE, ...) {
 
     cat("re2 pre-compiled regular expression\n\n")
-    if(is_re2c_na(x)){
+    na_status = is_re2c_na(x)
+    if(na_status){
         cat("NA string pattern\n")
     }
-    cat("pattern: ")
-    cat(get_pattern(x))
-    cat("\n")
-    cat("number of capturing subpatterns: ")
-    cat(get_number_of_groups(x))
-    cat("\n")
-    cat("capturing names with indices: \n")
-    print(get_named_groups(x))
+    cat_lines("pattern: ", get_pattern(x))
+    cat_lines("number of capturing subpatterns: ", get_number_of_groups(x))
+    cat_lines("capturing names with indices: \n", paste(get_named_groups(x)))
+    cat_lines("expression size: ", get_expression_size(x))
 
-    cat("expression size: ")
-    cat(get_expression_size(x))
-    cat("\n")
+    if(options && !na_status){
+        cat("\nOptions:\n\n")
+        opt = get_options(x)
+        cat_lines("UTF-8 pattern: ", opt$utf_8)
+        cat_lines("sase sensitive: ", opt$case_sensitive)
+        cat_lines("posix syntax: ", opt$posix_syntax)
+        if(opt$posix_syntax){
+            cat_lines("^$ only match beginning and end of text: ", opt$one_line)
+            cat_lines("perl classes: ", opt$perl_classes)
+            cat_lines("word boundary: ", opt$word_boundary)
+        }
+        cat_lines("dot match new line: ", opt$dot_nl)
+        cat_lines("literal pattern string: ", opt$literal)
+        cat_lines("longest match: ", opt$longest_match)
+        cat_lines("never match \\n: ", opt$never_nl)
+        cat_lines("never capture: ", opt$never_capture)
+        cat_lines("max memory: ", opt$max_mem)
+    }
     invisible(x)
 }
